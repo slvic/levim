0d0f81a408ba1e30f494d9d4762ebd0b3dd6402e
slvic <slvic@slvics-MacBook-Pro.local>
2025-06-27 02:28:16 +0300

    enable tree lol

--- init.lua
+++ init.lua
@@ -735,7 +735,9 @@ require('lazy').setup({
       }
     end,
   },
-
+  {
+    'airblade/vim-rooter',
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -977,7 +979,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
